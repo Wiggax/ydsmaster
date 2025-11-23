d1036a22f91763436b4cb5f6c854a0fe790e282e
Wiggax <burakuzunn03@gmail.com>
2025-11-23 23:48:36 +0300

    Increase body limit for data migration

--- server/index.js
+++ server/index.js
@@ -22,7 +22,8 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Request logging middleware
 app.use((req, res, next) => {
